3227712cdf6785d881c6eb07ac05be9c91383ec4
Bart Wiegmans <bartwiegmans@gmail.com>
2019-01-10 19:43:02 +0100

    [JIT] More rigorous type checking
    - actually implement a sensible type check
    - check type of expression return value with output operand, if any
    - check type of IF operands
    
    On the other hand, I'm not convinced we actually want a STORE_NUM
    operator.

--- src/core/ops.c
+++ src/core/ops.c
@@ -11750,7 +11750,7 @@ static const MVMOpInfo MVM_op_infos[] = {
         0,
         0,
         0,
-        { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_int16 }
+        { MVM_operand_write_reg | MVM_operand_int64, MVM_operand_int16 }
     },
     {
         MVM_OP_sp_getarg_n,
@@ -11764,7 +11764,7 @@ static const MVMOpInfo MVM_op_infos[] = {
         0,
         0,
         0,
-        { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_int16 }
+        { MVM_operand_write_reg | MVM_operand_num64, MVM_operand_int16 }
     },
     {
         MVM_OP_sp_getarg_s,
@@ -11778,7 +11778,7 @@ static const MVMOpInfo MVM_op_infos[] = {
         0,
         0,
         0,
-        { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_int16 }
+        { MVM_operand_write_reg | MVM_operand_str, MVM_operand_int16 }
     },
     {
         MVM_OP_sp_fastinvoke_v,
